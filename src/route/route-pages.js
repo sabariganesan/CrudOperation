5722057656442fc5915e68162e4576143b67b0ed
sabariganesan <sabariganesan01@gmail.com>
2022-02-16 13:48:35 +0530

    initial commit

---
+++ src/route/route-pages.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../style/variable.css"
+
+export function RoutePage() {
+  return (
+    <Router>
+        <Routes>
+          <Route />
+        </Routes>
+    </Router>
+  );
+}
