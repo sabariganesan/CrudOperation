edf97f338e72fdf362a2bd1263f1b607cdd591b0
sabariganesan <sabariganesan01@gmail.com>
2022-02-16 15:17:37 +0530

    home page name changed

--- src/route/route-pages.js
+++ src/route/route-pages.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { homepath } from "../consts/RoutePaths";
 import "../style/variable.css"
 
 export function RoutePage() {
