1850e8a38b9e7389560a5df3d50a108e2399162c
pradee <s.pradeesh4@gmail.com>
2022-02-18 11:59:30 +0530

    footer updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
 import { RoutePage } from "./route/route-pages";
 import reportWebVitals from "./reportWebVitals";
 import Footer from "../src/components/common/footer/Footer"
@@ -9,21 +8,6 @@ import Footer from "../src/components/common/footer/Footer"
 ReactDOM.render(
   <React.StrictMode>
       <Footer/>
-=======
-// import "./index.css";
-import { RoutePage } from "./route/route-pages";
-import reportWebVitals from "./reportWebVitals";
-import Contact from "./components/contact/Contact"
-
-ReactDOM.render(
-  <React.StrictMode>
-  
-    <Contact />
-
-    {/* <CommonContext.Provider>
-      <RoutePage />
-    </CommonContext.Provider> */}
->>>>>>> d9aaaf45f77b39c438423fd5fe365cb9b92105e8
   </React.StrictMode>,
   document.getElementById("root")
 );
